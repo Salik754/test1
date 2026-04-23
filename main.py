05e3ebbf2dd0a13e5373f2e878a9655df2115767
Salik754 <salikahmadn505@deltalearns.ca>
2026-04-23 13:14:17 -0700

    did the pseudocode
    yh

---
+++ main.py
@@ -0,0 +1,27 @@
+# Create an empty dictionary called 'robots'
+
+# prompt the user for name
+
+# PROMPT user for 'zone' (Downtown, Suburbs, Industrial)
+#         VALIDATE that 'zone' is correct; RE-PROMPT if not
+#         STORE 'name' as key and 'zone' as value in 'robots'
+
+#     PROMPT user for 'total_distance' (5-500)
+#     VALIDATE 'total_distance' is within range; RE-PROMPT if not
+    
+#     CREATE an empty dictionary 'cargo_weights'
+#     FOR each 'name' in 'robots':
+#         PROMPT for 'weight' (1-50 kg)
+#         VALIDATE 'weight' is within range; RE-PROMPT if not
+#         STORE 'weight' in 'cargo_weights' linked to 'name'
+        
+#     PROMPT for 'weather' (Clear, Rain, Storm)
+#     VALIDATE 'weather' is valid; RE-PROMPT if not
+    
+#     IF distance > 300 OR any weight > 50 OR weather is "Storm":
+#         PRINT "🚨 Deployment Unsafe!"
+#     ELSE:
+#         FOR each 'name' in 'robots':
+#             PRINT name, zone, and weight
+#         PRINT "🤖 Robots Ready for Delivery!"
+# END
